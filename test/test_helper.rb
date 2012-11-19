42a47460043889a62b6d4fa75ac7d9bbaa2f25e4
Matt Wildig <matt@mattwildig.co.uk>
2012-11-19 19:22:41 +0000

    Add css test assertions
    Add test assertions assert_css and refute_css, which assert (or refute)
    that a node matching the specified css string exists in the specified
    HTML.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,24 @@
 require "bundler/setup"
 require "haml"
 require "minitest/autorun"
+require "nokogiri"
 
 MiniTest::Unit::TestCase.send :include, Module.new {
   def render(text, options = {}, &block)
     scope  = options.delete(:scope)  || Object.new
     locals = options.delete(:locals) || {}
     Haml::Engine.new(text, options).to_html(scope, locals, &block)
+  end
+
+  def assert_css(css, html, msg=nil)
+    msg = message(msg) {"Expected #{mu_pp html} to match css selector #{mu_pp css}"}
+    fragment = Nokogiri::HTML.fragment(html)
+    assert fragment.at_css(css), msg
+  end
+
+  def refute_css(css, html, msg=nil)
+    msg = message(msg) {"Expected #{mu_pp html} to not match css selector #{mu_pp css}"}
+    fragment = Nokogiri::HTML.fragment(html)
+    refute fragment.at_css(css), msg
   end
 }
