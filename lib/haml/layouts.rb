f82c2286bf2ae9daf63e36a738b385b3d193f436
Matt Wildig <matt@mattwildig.co.uk>
2012-12-08 20:57:26 +0000

    Add some documentation for layouts.

--- lib/haml/layouts.rb
+++ lib/haml/layouts.rb
@@ -8,6 +8,19 @@ module Haml
 
   class Engine
 
+    # TODO: work out the best place to put this documentation.
+
+    # A simple layouts implementation.
+    #
+    # Renders the Haml template as normal, then renders the file specified
+    # by +options.layout+ (defaults to +layout.haml+) and inserts the original
+    # rendered file where +yield+ is called in the layout.
+    #
+    # Can also handle +content_for :sym+ blocks which will be inserted in the
+    # layout when +yield :sym+ is called, similar to Rails.
+    #
+    # To specify a different directory to look for layout files use the
+    # +layout_base_dir+ option, the default is the current working directory.
     def render_with_layout(scope = Object.new, locals = {}, &block)
       return render_without_layout(scope, locals, &block) unless options[:layout]
 
