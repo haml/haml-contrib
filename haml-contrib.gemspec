edd0cc1dc32fc05972052380bd6cb5197c0f2dbf
Dan Allen <dan.j.allen@gmail.com>
2013-07-28 19:54:42 -0600

    add filter for AsciiDoc markup

--- haml-contrib.gemspec
+++ haml-contrib.gemspec
@@ -13,5 +13,6 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "haml", ">= 3.2.0.alpha.13"
   spec.add_development_dependency "minitest"
+  spec.add_development_dependency "asciidoctor"
 
 end
---
+++ lib/haml/filters/asciidoc.rb
@@ -0,0 +1,4 @@
+Haml::Filters.register_tilt_filter 'AsciiDoc'
+Haml::Filters::AsciiDoc.options[:safe] = :safe
+Haml::Filters::AsciiDoc.options[:attributes] ||= {}
+Haml::Filters::AsciiDoc.options[:attributes]['notitle!'] = ''
